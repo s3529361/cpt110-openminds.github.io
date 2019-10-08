515a1076324253206347d3871d7be90122f84497
Lachlan <Lachlan@DESKTOP-8KFSBRA>
2019-10-08 18:07:37 +1100

    Removed old version of website, moved 'b' version to root.

--- js/site.js
+++ js/site.js
@@ -1,15 +1,55 @@
-
-
 /* Meet the team specifics */
+preloadImages("homepage-hero.jpg","lachlan-picture.jpg","logo.png");
 
-$(document).ready( function() {
-    $('.member-cell').click(function() {
+var memberTarget;
 
-        if (!$(this).hasClass("selected")) 
-        {
-            $(".selected").toggleClass("selected");
-            $(this).toggleClass("selected");
-        }
-})}
+
+
+$(window).on("load", function() {
+    
+    setTimeout(function() {$(".home-main").toggleClass("active")}, 1000)
+    setTimeout(function() {$(".home-enter").toggleClass("active").click(enterSite)}, 2000)
+
+}
 )
+
+function enterSite() 
+{
+    $(".home-main").toggleClass("active")
+    $(".home-enter").toggleClass("active")
+
+    $("header").toggleClass("inactive");
+    setTimeout(changePage("our-team"), 1000)
+}
+
+function changePage(pageTitle)
+{
+
+    $.ajax({
+        url: "partials/" + pageTitle + ".html",
+        cache: false,
+        context: document.main}).done(function(data) {
+            $("main").html(data).hide().removeClass("home-main").slideDown("slow");
+        })
+
+        switch(pageTitle) {
+            case "our-team":
+                    $(document.body).on('click', '.member-cell' ,function(){
+                        if (!$(this).hasClass("selected")) 
+                        {
+                        $(".selected").toggleClass("selected");
+                        $(this).toggleClass("selected");
+                        }
+                    })
+            break;
+        }
+    }
+
+    function preloadImages() {
+        for (var i = 0; i < arguments.length; i++) {
+          $("<img />").attr("src", "img/" + arguments[i]);
+        }
+      }
+
+
 
